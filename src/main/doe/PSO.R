ceb0b30cd1b9dd56f61552b903b5627fee0e4407
yannrichet <richetyann@gmail.com>
2022-10-24 15:23:03 +0200

    sync to new funz-profile/templr + add 1D test

--- src/main/doe/PSO.R
+++ src/main/doe/PSO.R
@@ -2,8 +2,8 @@
 #tags: Optimization
 #authors: Yann Richet <yann.richet@irsn.fr>, Claus Bendtsen <papyrus.bendtsen@gmail.com>
 #references: Clerc, M. et al. (2010) http://www.particleswarm.info/standard\_pso\_2007.c
-#require: future
-#options: maxit='10';seed='123'
+#require: future; templr
+#options: maxit='30';seed='123'
 #options.help: maxit=maximum number of iterations
 
 PSO <- function(options) {
@@ -19,59 +19,67 @@ PSO <- function(options) {
 getInitialDesign <- function(algorithm, input, output) {
     set.seed(algorithm$seed)
     algorithm$input <- input
-    algorithm$output <- output
-    d = length(input)
+    algorithm$output <- output  
+    algorithm$d <- length(input)
     library(future)
-    plan("multisession",workers=2)
-    algorithm$job = future(lazy = FALSE,{
-        sink(paste0('PSO_',algorithm$id,'.out'),type='output')
+    library(templr)
+    wd = getwd()
+    algorithm$job = future(evaluator=plan("multisession"),lazy = FALSE,{
+        sink(file.path(wd,paste0('PSO_',algorithm$id,'.out')),type='output')
+        print("Starting psoptim()")
         set.seed(algorithm$seed)
-        o = psoptim(par=rep(.5,d),fn=function(x) {ask.Y(id=algorithm$id,matrix(x,ncol=d))}, lower=rep(0,d), upper=rep(1,d),control=list(maxit=algorithm$maxit,vectorize=T))
+        o = psoptim(par=(min_input(input)+max_input(input))/2,
+                  fn=function(x) {
+                      ask_Y(id=algorithm$id,x=matrix(x,ncol=algorithm$d))}, 
+                  lower=min_input(input), upper=max_input(input), 
+                  control=list(maxit=algorithm$maxit,vectorized=T))
+        print("psoptim() ended")
         print(o)
         sink(type='output')
-        ask.Y(id=algorithm$id,NULL) # End of loop
+        ask_Y(id=algorithm$id,matrix(NaN,ncol=algorithm$d))
     })
-    
     algorithm$i = 0
     
     Sys.sleep(.1)
     
-    Xn = ask.X(id=algorithm$id)
+    Xn = ask_X(id=algorithm$id)
     algorithm$s = nrow(Xn)
 
-    names(Xn) <- names(algorithm$input)
-    return(from01(Xn,algorithm$input))
+    colnames(Xn) <- names(algorithm$input)
+    return(Xn)
 }
 
 getNextDesign <- function(algorithm, X, Y) {
-    #if (algorithm$maxit < algorithm$i) return(NULL)
     algorithm$i = algorithm$i + 1
     
     y = Y[(nrow(Y)-algorithm$s+1):nrow(Y),]
-    if (is.na(y)) return(NULL)
-    
-    tell.Y(id=algorithm$id,y)
+    if (all(is.na(y))) {
+        tell_Y(id=algorithm$id,NULL)
+        return(NULL)
+    } else tell_Y(id=algorithm$id,y)
     
     Sys.sleep(.1)
-    
-    Xn = ask.X(id=algorithm$id)
+
+    Xn = ask_X(id=algorithm$id)
     if (is.null(Xn)) return(NULL)
     algorithm$s = nrow(Xn)
 
-    names(Xn) <- names(algorithm$input)
-    return(from01(Xn,algorithm$input))
+    colnames(Xn) <- names(algorithm$input)
+    return(Xn)
 }
 
 displayResults <- function(algorithm, X, Y) {
-    X = X[-nrow(X),]
-    Y = Y[-nrow(Y),]
-    
-    algorithm$files <- "plot.png"
+    algorithm$files <- paste0("plot[",algorithm$i,"].png")
     png(file = algorithm$files, height = 600, width = 600)
-    red=(Y-min(Y))/(max(Y)-min(Y))
-    #pairs(X,col=rgb(r=red,g=0,b=1-red),Y=Y,d=nrow(X),panel=panel.vec)
-    pairs(X,col=rgb(r=red,g=0,b=1-red))
-    #pairs(cbind(X,Y))
+    if (ncol(X)>1) {
+        red=(Y-min(Y))/(max(Y)-min(Y))
+        #pairs(X,col=rgb(r=red,g=0,b=1-red),Y=Y,d=nrow(X),panel=panel.vec)
+        pairs(X,col=rgb(r=red,g=0,b=1-red))
+        #pairs(cbind(X,Y))
+    } else {
+        red=(Y-min(Y))/(max(Y)-min(Y))
+        plot(X,Y)
+    }
     dev.off()
     
     ## return HTML string containing plot image
@@ -120,7 +128,7 @@ psoptim <- function (par, fn, gr = NULL, ..., lower=-1, upper=1,
                 c.p = .5+log(2), c.g = .5+log(2), d = NA,
                 v.max = NA, rand.order = TRUE, max.restart=Inf,
                 maxit.stagnate = Inf,
-                vectorize=FALSE, hybrid = FALSE, hybrid.control = NULL,
+                vectorized=FALSE, hybrid = FALSE, hybrid.control = NULL,
                 trace.stats = FALSE, type = "SPSO2007")
     nmsC <- names(con)
     con[(namc <- names(control))] <- control
@@ -157,7 +165,7 @@ psoptim <- function (par, fn, gr = NULL, ..., lower=-1, upper=1,
     p.randorder <- as.logical(con[["rand.order"]]) # process particles in random order?
     p.maxrestart <- con[["max.restart"]] # maximal number of restarts
     p.maxstagnate <- con[["maxit.stagnate"]] # maximal number of iterations without improvement
-    p.vectorize <- as.logical(con[["vectorize"]]) # vectorize?
+    p.vectorized <- as.logical(con[["vectorized"]]) # vectorized?
     if (is.character(con[["hybrid"]])) {
         p.hybrid <- pmatch(con[["hybrid"]],c("off","on","improved"))-1
         if (is.na(p.hybrid)) stop("hybrid should be one of \"off\", \"on\", \"improved\"")
@@ -177,7 +185,7 @@ psoptim <- function (par, fn, gr = NULL, ..., lower=-1, upper=1,
                 signif(p.w1,4),", c.p=",signif(p.c.p,4),
                 ", c.g=",signif(p.c.g,4))
         message("v.max=",signif(con[["v.max"]],4),
-                ", d=",signif(p.d,4),", vectorize=",p.vectorize,
+                ", d=",signif(p.d,4),", vectorized=",p.vectorized,
                 ", hybrid=",c("off","on","improved")[p.hybrid+1])
         if (p.trace.stats) {
             stats.trace.it <- c()
@@ -188,7 +196,7 @@ psoptim <- function (par, fn, gr = NULL, ..., lower=-1, upper=1,
     }
     ## Initialization
     if (p.reltol!=0) p.reltol <- p.reltol*p.d
-    if (p.vectorize) {
+    if (p.vectorized) {
         lowerM <- matrix(lower,nrow=npar,ncol=p.s)
         upperM <- matrix(upper,nrow=npar,ncol=p.s)
     }
@@ -237,7 +245,7 @@ psoptim <- function (par, fn, gr = NULL, ..., lower=-1, upper=1,
             diag(links) <- TRUE
         }
         ## The swarm moves
-        if (!p.vectorize) {
+        if (!p.vectorized) {
             if (p.randorder) {
                 index <- sample(p.s)
             } else {
@@ -437,249 +445,3 @@ psoptim <- function (par, fn, gr = NULL, ..., lower=-1, upper=1,
     
     return(o)
 }
-
-######################################## [min,max] <-> [0,1] ########################################
-
-from01 = function(X, inp) {
-  nX = names(X)
-  for (i in 1:ncol(X)) {
-    namei = nX[i]
-    X[,i] = X[,i] * (inp[[ namei ]]$max-inp[[ namei ]]$min) + inp[[ namei ]]$min
-  }
-  return(X)
-}
-
-to01 = function(X, inp) {
-  nX = names(X)
-  for (i in 1:ncol(X)) {
-    namei = nX[i]
-    X[,i] = (X[,i] - inp[[ namei ]]$min) / (inp[[ namei ]]$max-inp[[ namei ]]$min)
-  }
-  return(X)
-}
-
-######################################## asktell_fun.R ########################################
-
-## These functions are emulating the ask/tell hack. If you put ask.Y function as argument instead of a true function (like sin), it will wait as long as you put the Y values using tell.Y() in the same directory. So a 2nd R session will be used to get the X values asked (using ask.X) and then call tell.Y() which will unlock first session. This finally allows an asynchronized IO between many R/matlab/... sessions.
-#' @author Y. Richet, from an idea by D. Sinoquet. Async IO principle was defined by G. Pujol.
-#' @test x=matrix(runif(10)); f=sin; print(f(x)); parallel::mcparallel({tell.Y(f(ask.X()))}); Sys.sleep(1); print(ask.Y(x));
-#' @test x=matrix(runif(10),ncol=2); f=function(X)rowSums(sin(X)); print(f(x)); parallel::mcparallel({tell.Y(f(ask.X()))}); Sys.sleep(1); print(ask.Y(x));
-#' @test x=matrix(runif(10),ncol=2); f=function(X)rowSums(sin(X)); print(f(x));library(future); plan("multisession",workers=2); future(lazy=FALSE,{tell.Y(f(ask.X()))}); Sys.sleep(1); print(ask.Y(x));
-#' @example f=sin; future::future(evaluator=plan("multisession"),{while(TRUE) {tell.Y(f(ask.X()))}}); optim(par=.5, fn=ask.Y, lower=0, upper=pi, method="L-BFGS-B")
-#' @example f=sin; parallel::mcparallel({while(TRUE) {tell.Y(f(ask.X()))}}); optim(par=.5, fn=ask.Y, lower=0, upper=pi, method="L-BFGS-B")
-#' @example optim(par=c(.5,.5), fn=function(x) ask.Y(matrix(x,ncol=2)), lower=c(0,0), upper=c(pi,pi), method="L-BFGS-B")
-
-.default_dev.path="/tmp"
-.default_dev.X  =  "X.todo"
-.default_dev.dX = "dX.todo"
-.default_dev.Y  =  "Y.done"
-.default_dev.dY = "dY.done"
-#dX = 0.001
-.trace = FALSE
-.sleep.step = 1
-.timeout = 3600*24 # 1 day before timeout
-
-ask.Y <- function(x, id=0, dev.X=.default_dev.X, dev.Y=.default_dev.Y, dev.path=NULL, sleep.step=.sleep.step, sleep.init=0, trace=ifelse(exists(".trace"),.trace,TRUE), timeout=.timeout) {
-    
-    if (trace) cat("?Y(",paste0(collapse = ",",x),") ")
-    
-    write.io(x,file = X.file(id,dev.X,dev.path))
-    
-    Sys.sleep(sleep.init)
-    t=0
-    lock=paste0("ask.Y_",id)
-    file.create(lock)
-    while (!file.exists(file = Y.file(id,dev.Y,dev.path)) & (timeout>0 & t<timeout)) {
-        Sys.sleep(sleep.step)
-        t=t+sleep.step
-        if (!file.exists(lock)) stop("ask.Y break !")
-        if (trace) cat(".")
-    }
-    file.remove(lock)
-    if (timeout>0 & t>=timeout) stop("ask.Y timeout !")
-    Sys.sleep(sleep.step)
-    if (trace) cat(",")
-    
-    y = read.io(file = Y.file(id,dev.Y,dev.path))
-    
-    if (trace) cat("(",paste0(collapse = ",",y),")")
-
-    return(y)
-}
-
-ask.dY <- function(x, dX=0.001, id=0, dev.dX=.default_dev.dX, dev.dY=.default_dev.dY, dev.path=NULL, sleep.step=.sleep.step, sleep.init=0, trace=ifelse(exists(".trace"),.trace,TRUE), timeout=.timeout) {
-    
-    if (trace) cat("?dY(",paste0(collapse = ",",x),") ")
-
-    d = ncol(x)
-    # build the finite difference X matrix
-    xdx = t(matrix(x,d,d+1))
-    for (i in 1:d) {
-        dx = dX
-        if (xdx[1+i,i] + dx > 1) {dx = -dx}
-        xdx[1+i,i] = xdx[1+i,i] + dx
-    }
-    
-    write.io(xdx,file = dX.file(id,dev.dX,dev.path))
-    
-    Sys.sleep(sleep.init)
-    t=0
-    lock=paste0("ask.dY_",id)
-    file.create(lock)
-    while (!file.exists(file = dY.file(id,dev.dY,dev.path)) & (timeout>0 & t<timeout)) {
-        Sys.sleep(sleep.step)
-        t=t+sleep.step
-        if (!file.exists(lock)) stop("ask.dY break !")
-        if (trace) cat(".")
-    }
-    file.remove(lock)
-    if (timeout>0 & t>=timeout) stop("ask.dY timeout !")
-    Sys.sleep(sleep.step)
-    if (trace) cat(",")
-    
-    ydy = read.io(file = dY.file(id,dev.dY,dev.path))
-    
-    # extract the finite differences for Y vector
-    dy = array(-1,d)
-    for (i in 1:d) {
-        dy[i] = (ydy[i+1] - ydy[1]) / (xdx[i+1,i] - xdx[1,i])
-    }
-    
-    if (trace) cat("(",paste0(collapse = ",",dy),")")
-    
-    return(dy)
-}
-
-
-#' @example while(x <- ask.X()) tell.Y(-sin(x))
-#' @example while(x <- ask.X()) tell.Y(-sin(x[,1]*cos(x[,2])))
-ask.X <- function(id=0, dev.X=.default_dev.X, dev.path=NULL, sleep.step=.sleep.step, sleep.init=0, trace=ifelse(exists(".trace"),.trace,TRUE), timeout=.timeout,clean = T) {
-    
-    if (trace) cat("?X ")
-    
-    Sys.sleep(sleep.init)
-    t=0
-    lock=paste0("ask.X_",id)
-    file.create(lock)
-    while (!file.exists(file = X.file(id,dev.X,dev.path)) & (timeout>0 & t<timeout)) {
-        Sys.sleep(sleep.step)
-        t=t+sleep.step
-        if (!file.exists(lock)) stop("ask.X break !")
-        if (trace) cat(":")
-    }
-    file.remove(lock)
-    if (timeout>0 & t>=timeout) stop("ask.X timeout !")
-    Sys.sleep(sleep.step)
-    if (trace) cat(";")
-    
-    x = read.io(file = X.file(id,dev.X,dev.path),clean = clean)
-    
-    if (trace) cat("(",paste0(collapse = ",",x),")")
-    
-    return(x)
-}
-
-ask.dX <- function(id=0, dev.dX=.default_dev.dX, dev.path=NULL, sleep.step=.sleep.step, sleep.init=0, trace=ifelse(exists(".trace"),.trace,TRUE), timeout=.timeout, clean=T) {
-    
-    if (trace) cat("?dX ")
-    
-    Sys.sleep(sleep.init)
-    t=0
-    lock=paste0("ask.dX_",id)
-    file.create(lock)
-    while (!file.exists(file = dX.file(id,dev.dX,dev.path)) & (timeout>0 & t<timeout)) {
-        Sys.sleep(sleep.step)
-        t=t+sleep.step
-        if (!file.exists(lock)) stop("ask.dX break !")
-        if (trace) cat(":")
-    }
-    file.remove(lock)
-    if (timeout>0 & t>=timeout) stop("ask.dX timeout !")
-    Sys.sleep(sleep.step)
-    if (trace) cat(";")
-    
-    dx = read.io(file = dX.file(id,dev.dX,dev.path),clean = clean)
-    
-    if (trace) cat(paste0(collapse = ",",dx))
-    
-    return(dx) #as.matrix(dx[,2:ncol(dx)]))
-}
-
-tell.Y <- function(y, id=0, dev.Y=.default_dev.Y, dev.path=NULL, trace=ifelse(exists(".trace"),.trace,TRUE)) {
-    if (trace) cat("!Y=",y)
-    
-    write.io(y,file = Y.file(id,dev.Y,dev.path))
-}
-
-tell.dY <- function(y, id=0, dev.dY=.default_dev.dY, dev.path=NULL, trace=ifelse(exists(".trace"),.trace,TRUE)) {
-    if (trace) cat("!dY=",y)
-    
-    write.io(y,file = dY.file(id,dev.dY,dev.path))
-}
-
-#' @test x=123;write.io(x,"x.dat");read.io("x.dat")
-#' @test x=matrix(c(123,456),ncol=2);write.io(x,"x.dat");read.io("x.dat")
-#' @test x=matrix(c(123,456,789,101),ncol=2);write.io(x,"x.dat");read.io("x.dat")
-write.io <- function(data,file, trace=ifelse(exists(".trace"),.trace,TRUE)) {
-    i=0
-    while(file.exists(file) & i<100) {Sys.sleep(0.05); i=i+1; if (trace) cat(" ")}
-    if (i>=100) stop("file ",file, " already exists !")
-    if (trace) cat(">")
-    write.table(data,file=file,row.names=FALSE)
-}
-
-read.io <- function(file,clean=TRUE, trace=ifelse(exists(".trace"),.trace,TRUE)) {
-    t = NULL;
-    i=0
-    try(t <- as.matrix(read.table(file=file,header=TRUE)),silent=TRUE)
-    while(is.null(t) & i<10) {
-        Sys.sleep(0.05)
-        i=i+1
-        if (trace) cat(" ")
-        try(t <- as.matrix(read.table(file=file,header=TRUE)),silent=TRUE)
-    }
-    if (is.null(t) & i<10 & trace) cat("\n:)\n")
-    #t <- as.matrix(read.table(file=file,header=TRUE))
-    if (clean) {file.remove(file); if (trace) cat("-")}
-    if (trace) cat("<")
-    return(t)
-}
-
-X.file <- function(id=0,dev.X=.default_dev.X,dev.path=.default_dev.path) {
-    if (!is.null(dev.path)) {
-        dev.X = file.path(dev.path,dev.X)
-    }
-    X.file = paste(sep="_",dev.X,id)
-    #if(isTRUE(.trace)) print(X.file)
-    
-    return(X.file)
-}
-
-dX.file <- function(id=0,dev.dX=.default_dev.dX,dev.path=.default_dev.path) {
-    if (!is.null(dev.path)) {
-        dev.dX = file.path(dev.path,dev.dX)
-    }
-    dX.file = paste(sep="_",dev.dX,id)
-    
-    return(dX.file)
-}
-
-Y.file <- function(id=0,dev.Y=.default_dev.Y,dev.path=.default_dev.path) {
-    if (!is.null(dev.path)) {
-        dev.Y = file.path(dev.path,dev.Y)
-    }
-    Y.file = paste(sep="_",dev.Y,id)
-    #if(isTRUE(.trace)) print(Y.file)
-    
-    return(Y.file)
-}
-
-dY.file <- function(id=0,dev.dY=.default_dev.dY,dev.path=NULL) {
-    if (!is.null(dev.path)) {
-        dev.dY = file.path(dev.path,dev.dY)
-    }
-    dY.file = paste(sep="_",dev.dY,id)
-    
-    return(dY.file)
-}
-
--- src/test/cases/branin1.R
+++ src/test/cases/branin1.R
@@ -7,26 +7,23 @@
 
 f <- function(x) {
 	x1 <- x[1]*15-5   
-	x2 <- x[2]*15     
+	x2 <- 0.75 #x[2]*15     
 	#matrix(
         (x2 - 5/(4*pi^2)*(x1^2) + 5/pi*x1 - 6)^2 + 10*(1 - 1/(8*pi))*cos(x1) + 10
     #,ncol=1)
 }
 input.f = list(
-    x1=list(min=0,max=1),
-    x2=list(min=0,max=1)
+    x1=list(min=0,max=1)#,x2=list(min=0,max=1)
 )
-output.f = "branin"
-argmin1.f = c(0.9616520, 0.15)
-argmin2.f = c(0.1238946, 0.8166644)
-argmin3.f = c(0.5427730, 0.15)
-min.f = 0.3978874
+output.f = "branin1"
+#optimise(f,interval = c(0,1))
+argmin.f = 0.5566755
+min.f = 2.400338
 
 library(testthat)
-if (!isTRUE(test_that("f(armgin.f) == f.min",{expect_equal(f(argmin1.f),min.f,tolerance = .0001)}))) quit(status=1)
+if (!isTRUE(test_that("f(armgin.f) == f.min",{expect_equal(f(argmin.f),min.f,tolerance = .0001)}))) quit(status=1)
 
 test = function(algorithm_file) {
     results = run.algorithm(algorithm_file, options=list(maxit='30'),fun=list(input=input.f,output=output.f,fun=f))
     if (!isTRUE(test_that("branin min",{expect_equal(as.numeric(results$min),min.f,tolerance = .01)}))) quit(status=1)
 }
-
